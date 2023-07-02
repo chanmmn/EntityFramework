c881c1cb50b02e00e0d5f3520d6a851dcf7d1c2c
chanmmn <33271651+chanmmn@users.noreply.github.com>
2023-07-02 23:41:52 +0800

    Commit ConAppEntityFrameworkTemplate
    Commit ConAppEntityFrameworkTemplate

---
+++ ConAppEntityFrameworkTemplate/ConAppEntityFrameworkTemplate/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ConAppRandomState
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, Start!");
+            // Scaffold-DbContext "Server=.\SQLExpress;Database=northwind;Trusted_Connection=True;TrustServerCertificate=true" Microsoft.EntityFrameworkCore.SqlServer -OutputDir Models -t Products -f
+        }
+
+        public static void InsertRandom()
+        {
+
+        }
+    }
+}
